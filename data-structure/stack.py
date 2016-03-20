27e502e17cb4c4cdadb866989f3bdbf673caeba2
Cigarent <xiaojunh1996@hotmail.com>
2016-03-20 09:01:26 -0500

    implement stack with py

---
+++ data-structure/stack.py
@@ -0,0 +1,16 @@
+class stack(object):
+	def __init__(self):
+		self.size = 0
+		self.stack = []
+	def isEmpty():
+		return True if self.size==0 else False
+	def peek():
+		return self.stack[size-1] if self.isEmpty()==False else False
+	def push(i):
+		self.stack.append(i)
+		self.size+=1
+	def pop():
+		return False if self.isEmpty()
+		size -= 1
+		return self.stack.pop()
+	
