194427eed3c751e6b74df98d13e101759c257899
Cigarent <xiaojunh1996@hotmail.com>
2016-03-20 09:06:38 -0500

    stack&queue

---
+++ data-structure/stack_queue.py
@@ -0,0 +1,16 @@
+'''
+Actually there's a easier way to implement stack and queue in python
+'''
+
+stack = []
+queue = []
+
+#push
+stack.append()
+queue.append()
+
+#pop, will cause error if nothing's in it
+stack.pop()
+queue.popleft()
+
+
